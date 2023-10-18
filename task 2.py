ae300fd94e3d4e8daa4170592b42ff064e9873c8
Talha ahmad <144198788+Talhaaa12@users.noreply.github.com>
2023-10-18 10:37:27 +0500

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,39 @@
+data_functions = {
+    'list_functions': [
+        ('append', list.append),
+        ('extend', list.extend),
+        ('insert', list.insert),
+        ('remove', list.remove),
+        ('pop', list.pop),
+        ('index', list.index),
+        ('count', list.count),
+        ('sort', list.sort),
+        ('reverse', list.reverse)
+    ],
+    'dict_functions': [
+        ('keys', dict.keys),
+        ('values', dict.values),
+        ('items', dict.items),
+        ('get', dict.get),
+        ('pop', dict.pop),
+        ('popitem', dict.popitem),
+        ('update', dict.update),
+        ('clear', dict.clear)
+    ],
+    'set_functions': [
+        ('add', set.add),
+        ('remove', set.remove),
+        ('discard', set.discard),
+        ('pop', set.pop),
+        ('clear', set.clear),
+        ('union', set.union),
+        ('intersection', set.intersection),
+        ('difference', set.difference),
+        ('symmetric_difference', set.symmetric_difference),
+        ('update', set.update)
+    ],
+    'tuple_functions': [
+        ('count', tuple.count),
+        ('index', tuple.index)
+    ]
+}
